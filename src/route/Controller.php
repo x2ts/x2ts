c2f6909e4cbdc54abebe73e3e82a5945efb0d784
TingSong-Syu <rek@rek.me>
2017-12-14 22:02:45 +0800

    add an implementation of SimpleController.

---
+++ src/route/Controller.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2017/12/5
+ * Time: 下午6:42
+ */
+
+namespace x2ts\route;
+
+use ReflectionMethod;
+use ReflectionParameter;
+use Throwable;
+use x2ts\ComponentFactory as X;
+use x2ts\route\event\PostActionEvent;
+use x2ts\route\event\PostRunEvent;
+use x2ts\route\event\PreActionEvent;
+use x2ts\route\event\PreRunEvent;
+use x2ts\Toolkit;
+
+abstract class Controller extends Action {
+    public function run(array $args = []) {
+        X::logger()->trace('App Start: '
+            . $this->server('REQUEST_METHOD') . ' '
+            . $this->server('REQUEST_URI')
+        );
+        X::bus()->dispatch(new PreActionEvent([
+            'dispatcher' => $this,
+            'action'     => $this,
+        ]));
+        try {
+            X::bus()->dispatch(new PreRunEvent([
+                'dispatcher' => $this,
+                'action'     => $this,
+            ]));
+            $this->_run($args[0] ?? '', $args[1] ?? '');
+            X::bus()->dispatch(new PostRunEvent([
+                'dispatcher' => $this,
+                'action'     => $this,
+            ]));
+        } catch (ApplicationExitException $e) {
+            if ($m = $e->getMessage()) {
+                X::logger()->trace('App end with message ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+            } else {
+                X::logger()->trace('App end without message' . "\n" . $e->getTraceAsString());
+            }
+        } catch (Throwable $e) {
+            X::logger()->error($e);
+        }
+        X::bus()->dispatch(new PostActionEvent([
+            'dispatcher' => $this,
+            'action'     => $this,
+        ]));
+        $this->response->response();
+        X::logger()->trace('App Exit: '
+            . $this->server('REQUEST_METHOD') . ' ' . $this->server('REQUEST_URI')
+        );
+    }
+
+    /**
+     * @param string $action
+     * @param string $suffix
+     */
+    private function _run(string $action, string $suffix) {
+        $method = Toolkit::toCamelCase(
+            'http_' . $this->server('REQUEST_METHOD') . '_' . $action);
+        if (!method_exists($this, $method)) {
+            if ($this->server('REQUEST_METHOD') !== 'HEAD' ||
+                !method_exists($this, $method = Toolkit::toCamelCase("http_get_$action"))) {
+                $this->setStatus(404)->out('Not Found');
+                return;
+            }
+        }
+        $rf = new ReflectionMethod($this, $method);
+        $params = $rf->getParameters();
+        $args = [];
+        foreach ($params as $param) {
+            /** @var ReflectionParameter $param */
+            $name = $param->getName();
+            if (null !== $this->request->query($name)) {
+                $args[] = $this->request->query($name);
+            } else if ($param->isOptional()) {
+                $args[] = $param->getDefaultValue();
+            } else {
+                $this->setStatus(400)->out("missing query parameter $name");
+                return;
+            }
+        }
+        $rf->invokeArgs($this, $args);
+    }
+}
---
+++ src/route/rule/SimpleController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2017/12/5
+ * Time: 下午6:45
+ */
+
+namespace x2ts\route\rule;
+
+
+use x2ts\route\Action;
+use x2ts\Toolkit;
+
+class SimpleController implements IRule {
+    protected $_conf = [
+        'defaultController' => 'index',
+        'defaultAction'     => 'index',
+    ];
+
+    public function getConf(): array {
+        return $this->_conf;
+    }
+
+    public function setConf(array $conf) {
+        $this->_conf = $conf;
+    }
+
+    protected $controller;
+
+    protected $actionName;
+
+    protected $suffix;
+
+    public function isMatch(string $uri): bool {
+        list($path) = explode('?', $uri, 2);
+        $lastDotPos = strrpos($path, '.');
+        $this->suffix = '';
+        if (false !== $lastDotPos) {
+            $this->suffix = substr($path, $lastDotPos + 1);
+            $path = substr($path, 0, $lastDotPos);
+        }
+        $parts = explode('/', trim($path, '/'));
+        if (count($parts) < 2) { // at least two parts: controller/action
+            return false;
+        }
+        $this->actionName = array_pop($parts);
+        $controllerName =& $parts[count($parts) - 1];
+        $controllerName = Toolkit::toCamelCase($controllerName, true);;
+        $classFullName = 'action\\' . implode('\\', $parts);
+        if (class_exists($classFullName)) {
+            $this->controller = new $classFullName();
+        } elseif (($classFullName .= 'Controller') && class_exists($classFullName)) {
+            $this->controller = new $classFullName();
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public function fetchAction(): Action {
+        return $this->controller;
+    }
+
+    public function fetchArguments(): array {
+        return [$this->actionName, $this->suffix];
+    }
+}
