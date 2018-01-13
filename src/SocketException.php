ac642844e5d9d22a196595654734315fca212c8b
TingSong-Syu <rek@rek.me>
2018-01-13 11:04:05 +0800

    Add SocketException and SocketTimeoutException

---
+++ src/SocketException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2018/1/12
+ * Time: 下午5:02
+ */
+
+namespace x2ts;
+
+
+class SocketException extends \Exception {
+}
---
+++ src/SocketTimeoutException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2018/1/12
+ * Time: 下午5:33
+ */
+
+namespace x2ts;
+
+
+class SocketTimeoutException extends SocketException {
+}
